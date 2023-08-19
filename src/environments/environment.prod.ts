1d910a42d2976a25ffba9662e05b0cba3e3e0ebf
YoungCatChen <YoungCatChen@users.noreply.github.com>
2023-08-18 17:28:42 -0700

    Added Google api auth as environments.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+
+  // Doesn't matter if stolen. Restriction is applied.
+  CLIENT_ID: '66208330263-1golbeif7s0v22alm171v8fbj2tpem0k.apps.googleusercontent.com',
+  API_KEY: 'AIzaSyD3sqirwKZ8bp8V9PENQI0QnsMtPMuIa8g',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  CLIENT_ID: 'invalid client id',
+  API_KEY: 'invalid api key',
+};
