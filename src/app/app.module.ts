f729c7bf5873502e832f2e1bfae453ac29348bf7
YoungCatChen <YoungCatChen@users.noreply.github.com>
2023-09-01 15:18:46 -0700

    Added angular material and its toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -7,7 +9,12 @@ import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
