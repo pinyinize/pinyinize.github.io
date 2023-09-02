1a014d5543a44fcd29ac4b60a3c9137faf2d8304
YoungCatChen <YoungCatChen@users.noreply.github.com>
2023-09-01 17:10:47 -0700

    fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 
 import {AppComponent} from './app.component';
+import {AppModule} from './app.module';
 
 
 describe('AppComponent', () => {
-  beforeEach(
-      () => TestBed.configureTestingModule(
-          {imports: [RouterTestingModule], declarations: [AppComponent]}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule, AppModule],
+    declarations: [AppComponent],
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
